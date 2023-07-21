2603850a0a3fc5152e6c3bd8cd549e36cb6546c7
ncip48 <mbahcip00@gmail.com>
2023-07-21 19:48:36 +0700

    fix tampilan

--- src/components/container/index.js
+++ src/components/container/index.js
@@ -5,11 +5,11 @@ export default function Container({ children, center }) {
     <div className={`container`}>
       {center ? (
         <div className="row justify-content-center vh-100 align-items-center">
-          <div className="col-md-6">{children}</div>
+          <div className="col-xl-4 col-lg-6 col-md-8">{children}</div>
         </div>
       ) : (
-        <div className="row">
-          <div className="col-md-6">{children}</div>
+        <div className="row justify-content-center">
+          <div className="col-xl-4 col-lg-6 col-md-8">{children}</div>
         </div>
       )}
     </div>
