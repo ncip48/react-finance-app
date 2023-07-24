344dfb32e611c3751f1e360ffe11f969b15a6a83
ncip48 <mbahcip00@gmail.com>
2023-07-24 12:39:16 +0700

    menu

--- src/components/menuItem/index.js
+++ src/components/menuItem/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Icon from "../icon";
+import { useNavigate } from "react-router-dom";
 
-export default function index({ title, icon, index }) {
+export default function MenuItem({ title, icon, index }) {
+  const navigate = useNavigate();
   return (
-    <div className={`col-3 ${index < 4 ? "mb-3" : ""} my-1`}>
+    <div
+      className={`col-3 ${index < 4 ? "mb-3" : ""} my-1`}
+      onClick={() => navigate("/worked")}
+    >
       <div className="d-flex align-items-center flex-column">
         <Icon name={icon} size="2rem" />
         <small
